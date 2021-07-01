00a8d698e58a572bb1924527588f95fb8ddb0048
Rob Culliton <rob.culliton@gmail.com>
2021-07-01 16:29:29 -0400

    rest of functions from chapter 5

--- attic.lisp
+++ attic.lisp
@@ -2,7 +2,6 @@
                         (garden (you are in a beautiful garden. there is a well in front of you))
                         (attic (you are in the attic. there is a giant welding torch in the corner.))))
 
-
 (defparameter *edges* '((living-room (garden west door)
                                     (attic upstairs ladder))
                        (garden (living-room east door))
@@ -18,7 +17,6 @@
 (defun describe-location (location nodes)
   (cadr (assoc location nodes)))
 
-
 (defun describe-path (edge)
   `(there is a , (caddr edge) going , (cadr edge) from here.))
 
@@ -31,15 +29,32 @@
     (remove-if-not #'at-loc-p objs)))
 
 
-(cadr (assoc 'whiskey *object-locations*))
-
-(objects-at 'garden *objects* *object-locations*)
-
-
-
 (defun describe-objects (loc objs obj-locs)
   (labels ((describe-obj (obj)
              `(you see a ,obj on the floor.)))
     (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-locs)))))
 
-(describe-objects 'garden *objects* *object-locations*)
+(defparameter *location* 'living-room)
+
+(defun look ()
+  (append (describe-location *location* *nodes*)
+          (describe-paths *location* *edges*)
+          (describe-objects *location* *objects* *object-locations*)))
+
+(defun walk (direction)
+  (let ((next (find direction
+                    (cdr (assoc *location* *edges*)) :key #'cadr)))
+    (if next
+        (progn (setf *location* (car next))
+               (look))
+        '(you cannot go that way.))))
+
+(defun pickup (object)
+  (cond ((member object
+                 (objects-at *location* *objects* *object-locations*))
+         (push (list object 'body) *object-locations*)
+         `(you are now carrying the ,object))
+        (t '(you cannot get that.))))
+
+(defun inventory ()
+  (cons 'items- (objects-at 'body *objects* *object-locations*)))
