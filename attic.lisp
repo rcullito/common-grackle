4d56fa86d20fa52707a740ef2a87a66bd94c30ef
Rob Culliton <rob.culliton@gmail.com>
2021-07-02 09:29:06 -0400

    some reading functions and a trivial custom lisp repl

--- attic.lisp
+++ attic.lisp
@@ -58,3 +58,44 @@
 
 (defun inventory ()
   (cons 'items- (objects-at 'body *objects* *object-locations*)))
+
+
+(defun say-hello ()
+  (princ "Please type your name:")
+  (let ((name (read-line)))
+    (princ "Nice to meet you, ")
+    (princ name)))
+
+(defun add-five ()
+  (print "please enter a number:")
+  (let ((num (read)))
+    (print "When I add five I get")
+    (print (+ num 5))))
+
+(progn (princ "This sentence will be interrupted")
+       (princ #\newline)
+       (princ "By a newline character"))
+
+
+(defparameter *foo* '(+ 1 2))
+
+
+(defun game-repl ()
+  (let ((cmd (game-read)))
+    (unless (eq (car cmd) 'quit)
+      (game-print (game-eval cmd))
+      (game-repl))))
+
+
+(defun game-read ()
+  (let ((cmd (read-from-string
+              (concatenate 'string "(" (read-line) ")"))))
+    (flet ((quote-it (x)
+             (list 'quote x)))
+      (cons (car cmd) (mapcar #'quote-it (cdr cmd))))))
+
+;; (game-read)
+
+
+
+
