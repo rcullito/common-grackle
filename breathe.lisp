e0bead079382f7a21b8c7bcb2f1f38117f648dd4
Rob Culliton <rob.culliton@gmail.com>
2021-07-15 16:36:48 -0400

    clean up the left and right column code into their own defuns

--- breathe.lisp
+++ breathe.lisp
@@ -1,10 +1,18 @@
 (defparameter *gap* "    ")
 
+(defun left-column (txt delim)
+  (let ((end-of-left-column (search delim txt)))
+    (subseq txt 0 end-of-left-column)))
+
+(defun right-column (txt delim)
+  (let* ((adjustment (length *gap*))
+        (beginning-of-right-column (+ (search delim txt :from-end :backward)
+                                      adjustment)))
+    (subseq txt beginning-of-right-column)))
+
 (with-open-file (my-stream "sample.txt" :direction :input)
   ;; todo split on blank space, and acc the left in one var and the right in another
   (let* ((current-line (read-line my-stream))
-         (end-of-left-column (search *gap*  current-line))
-         (beginning-of-right-column (+ (search *gap* current-line :from-end :backward) (length *gap*)))
-         (first-line (subseq current-line 0 end-of-left-column))
-         (second-line (subseq current-line beginning-of-right-column)))
+         (first-line (left-column current-line *gap*))
+         (second-line (right-column current-line *gap*)))
     (cons first-line second-line)))
