8ba0a61cb09185dada80213488ea1de8f857d5b9
Rob Culliton <rob.culliton@gmail.com>
2021-07-15 16:27:00 -0400

    return a cons cell of the first line and the second line of a page

--- breathe.lisp
+++ breathe.lisp
@@ -1,3 +1,5 @@
+(defparameter *gap* "    ")
+
 (with-open-file (my-stream "sample.txt" :direction :input)
   ;; todo split on blank space, and acc the left in one var and the right in another
   (position #\space (read-line my-stream)))
@@ -10,5 +12,8 @@
 (with-open-file (my-stream "sample.txt" :direction :input)
   ;; todo split on blank space, and acc the left in one var and the right in another
   (let* ((current-line (read-line my-stream))
-         (end-of-left-column (search "    "  current-line)))
-    (subseq current-line 0 end-of-left-column)))
+         (end-of-left-column (search "    "  current-line))
+         (beginning-of-right-column (+ (search *gap* current-line :from-end :backward) (length *gap*)))
+         (first-line (subseq current-line 0 end-of-left-column))
+         (second-line (subseq current-line beginning-of-right-column)))
+    (cons first-line second-line)))
