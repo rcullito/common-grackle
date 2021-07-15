6a736b61c9364018b5840730b5c662ea7fa7e3f6
Rob Culliton <rob.culliton@gmail.com>
2021-07-15 16:57:25 -0400

    works to get some samples just wrapping the entire let block in a loop, add a todo about where we need to work in order to handle the case of only a left column for a given line

--- breathe.lisp
+++ breathe.lisp
@@ -6,13 +6,19 @@
 
 (defun right-column (txt delim)
   (let* ((adjustment (length *gap*))
+         ;; TODO, need to add a case here for the line
+         ;; after W. Fritts
+         ;; where we will not find anything from a backwards search
+         ;; because there is only a first column
         (beginning-of-right-column (+ (search delim txt :from-end :backward)
                                       adjustment)))
     (subseq txt beginning-of-right-column)))
 
 (with-open-file (my-stream "sample.txt" :direction :input)
   ;; todo split on blank space, and acc the left in one var and the right in another
-  (let* ((current-line (read-line my-stream))
-         (first-line (left-column current-line *gap*))
-         (second-line (right-column current-line *gap*)))
-    (cons first-line second-line)))
+  (loop
+    (let ((current-line (read-line my-stream)))
+     (when current-line
+       (let* ((first-line (left-column current-line *gap*))
+              (second-line (right-column current-line *gap*)))
+         (print second-line))))))
