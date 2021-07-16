df3a8f7174089749f4ac9a18a57c9bbde2da2f24
Rob Culliton <rob.culliton@gmail.com>
2021-07-16 08:12:21 -0400

    much better, working print of second column. now just need to remove the nils

--- breathe.lisp
+++ breathe.lisp
@@ -1,24 +1,21 @@
-(defparameter *gap* "    ")
+(defparameter *delimeter* "    ")
 
 (defun left-column (txt delim)
   (let ((end-of-left-column (search delim txt)))
     (subseq txt 0 end-of-left-column)))
 
 (defun right-column (txt delim)
-  (let* ((adjustment (length *gap*))
-         ;; TODO, need to add a case here for the line
-         ;; after W. Fritts
-         ;; where we will not find anything from a backwards search
-         ;; because there is only a first column
-        (beginning-of-right-column (+ (search delim txt :from-end :backward)
-                                      adjustment)))
-    (subseq txt beginning-of-right-column)))
+  (let ((adjustment (length delim))
+        (backwards-search-position (search delim txt :from-end :backward)))
+    (when backwards-search-position
+      (let ((beginning-of-right-column (+ backwards-search-position adjustment)))
+            (subseq txt beginning-of-right-column)))))
 
 (with-open-file (my-stream "sample.txt" :direction :input)
   ;; todo split on blank space, and acc the left in one var and the right in another
   (loop
     (let ((current-line (read-line my-stream)))
      (when current-line
-       (let* ((first-line (left-column current-line *gap*))
-              (second-line (right-column current-line *gap*)))
+       (let* ((first-line (left-column current-line *delimeter*))
+              (second-line (right-column current-line *delimeter*)))
          (print second-line))))))
